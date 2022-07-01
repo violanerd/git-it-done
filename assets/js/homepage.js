fda35fec1fac23bcbc1f489ccb1a48269c2fe03d
violanerd <madalyne.cross@gmail.com>
2022-07-01 14:47:23 -0500

    added feature search by langugae

--- assets/js/homepage.js
+++ assets/js/homepage.js
@@ -2,6 +2,7 @@ var userFormEl = document.querySelector("#user-form");
 var nameInputEl = document.querySelector("#username");
 var repoContainerEl = document.querySelector("#repos-container");
 var repoSearchTerm = document.querySelector("#repo-search-term");
+var languageButtonsEl = document.querySelector("#language-buttons");
 
 var formSubmitHandler = function(event){
     event.preventDefault();
@@ -37,6 +38,19 @@ var getUserRepos = function (user) {
     });  
 }
 
+var getFeaturedRepos = function(language) {
+    var apiUrl = "https://api.github.com/search/repositories?q=" + language + "is:featured&sort=help-wanted-issues";
+
+    fetch(apiUrl).then(function(response) {
+        if (response.ok){
+            response.json().then(function(data) {
+                displayRepos(data.items, language);
+            });
+        } else {
+            alert("Error: Github User Not Found");
+        }
+    });
+};
 var displayRepos = function (repos, searchTerm) {
     if (repos.length === 0) {
         repoContainerEl.textContent="No repositories found."
@@ -78,4 +92,15 @@ var displayRepos = function (repos, searchTerm) {
     }
 }
 
+var buttonClickHandler = function (event){
+    var language = event.target.getAttribute("data-language");
+    if (language){
+        getFeaturedRepos(language);
+    }
+    // clear old content
+    repoContainerEl.textContent="";
+
+}
+
 userFormEl.addEventListener("submit", formSubmitHandler);
+languageButtonsEl.addEventListener("click", buttonClickHandler);
